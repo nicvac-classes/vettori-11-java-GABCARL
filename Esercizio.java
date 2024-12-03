432ae268b252f5e0776929f2f05f797550affe29
GABCARL <gabriele.carlone@marconibari.edu.it>
2024-12-03 16:06:26 +0000

    consegnato

--- Esercizio.java
+++ Esercizio.java
@@ -8,18 +8,50 @@ class Esercizio {
     // Il programma parte con una chiamata a main().
     public static void main(String args[])
     {
-        //Variabili del programma
-        String nome;
+        int n=0;
+        System.out.println("inserisci un numero");
+        n=scanner.nextInt();
+        int u[n];
+        u=new int [n];
+        for(i=0;i<n-1;i++){
+            if(n>=0){
+                System.out.println("u[i]");
+            }else{
+                while(n<0){
+                    System.out.println("inserire numero positivo");
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+                }
+            }
+            int r[n];
+        r =new int [n];
+        for(i=0;i<n-1;i++){
+            r[i]=random(11);
+            System.out.println("r[i]");
+        }
+        int s[n];
+        s=new int [n];
+        for(i=0;i<n-1;i++){
+            s[i]=random(6)+3;
+            System.out.println("s[i]");
+        }
+        int m[n];
+        m=new int [n];
+        for(i=0;i<n-1;i++){
+            media=u[i]+r[i]+s[i]/3
+            System.out.println("m[i]");
+        }
+        System.out.println("inseire un numero intero");
+        t=scanner.nextinput();
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        
+    }
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-1;j++){
+            if(u[i]-u[j]=t){
+                System.out.println("il risultato è ",t);
+                
+            }
+        }
     }
 }
 
